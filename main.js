231f3404e91a9d590f28006378f5b49a5b68a225
Brendan Oliver <brendanoliver001@gmail.com>
2022-12-22 11:51:59 -0500

    added menu btn open close funcitonality

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ faqs.forEach(faq => {
           }
     })
 });
+
+
+
+//toggle show/hide nav menu
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close nav menu
+
+const closeNav = document.querySelector("#close-menu-btn");       
+closeNav.addEventListener('click', () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+})
